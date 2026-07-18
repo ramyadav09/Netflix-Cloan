bb8b4c169afd4a5d11b7daaa2998bbc4ba568a9d
Ram Kumar Yadav <ramyadav2060@gmail.com>
2026-07-18 11:56:59 +0530

    bug fixed

--- src/components/movieReviewCard.jsx
+++ src/components/movieReviewCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import MovieReviewDetails from "./movieReviewDetails";
+import MovieReviewDetails from "./MovieReviewDetails";
 import VideoPlayer from "./VideoPlayer";
 import { Link, useParams } from "react-router-dom";
 import useMovieReview from "../hooks/useMovieReview";
@@ -20,9 +20,9 @@ const MovieReviewCard = () => {
       <MovieReviewDetails resId={resId} handleTrailer={handleTrailerPlaying} />
       {isTrailer && (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5 h-96 z-10">
-          <VideoPlayer 
-            movieId={resId} 
-            isFullScreen={false} 
+          <VideoPlayer
+            movieId={resId}
+            isFullScreen={false}
             onClose={() => setIsTrailer(false)}
           />
         </div>
